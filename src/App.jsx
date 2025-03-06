c6d6eef37bd4cddb405b1547c487c393bc9e8784
kbeske <beske.k@husky.neu.edu>
2025-03-05 19:13:59 -0700

    more visual cleanup, added sass

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import './App.css';
+import './App.scss';
 import Select from 'react-select';
 import { peopleDescriptions } from './peopleDescriptions';
 import { coinTypes } from './coinTypes';
