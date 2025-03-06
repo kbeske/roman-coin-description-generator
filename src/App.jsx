846fab305e87f39ae44d845f64c93045b417731a
kbeske <beske.k@husky.neu.edu>
2025-03-05 18:55:32 -0700

    formatting, add copy text button

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
 import Select from 'react-select';
 import { peopleDescriptions } from './peopleDescriptions';
@@ -11,13 +11,15 @@ function App() {
   const [coinType, setCoinType] = useState(coinTypes[1]);
   const [grade, setGrade] = useState(grades[5]);
   const [flaws, setFlaws] = useState([flawDescriptions[0]]);
-  const [description, setDescription] = useState('Hello, I am the description placeholder!');
+  const [description, setDescription] = useState('Hello, I am the description placeholder!\n\n' +
+    'Use the select dropdowns above to choose the attributes of the coin you\'re looking at.  ' +
+    'Then click the button to see a full description with the person, coin type, and more!  ' +
+    'You\'ll need to add the obverse/reverse descriptions to the listing description separately.');
+  const [isCopied, setIsCopied] = useState(false);
+  const [copyButtonClass, setCopyButtonClass] = useState('copy-button');
+  const [copyButtonText, setCopyButtonText] = useState('Copy Description');
 
   const generateDescription = () => {
-    console.log(person.label)
-    console.log(coinType.label)
-    console.log(flaws)
-
     var flawText = '';
     flaws.forEach(flaw => {
       flawText += `${flaw.value && `  `}${flaw.value}`;
@@ -43,43 +45,79 @@ function App() {
       {gradeTextSecondHalf}
     </div>
 
-    const desc2 = <span>BOO!</span>;
     setDescription(desc);
   }
 
+  const copyButtonOnClick = () => {
+    navigator.clipboard.writeText(description);
+    setIsCopied(true)
+  };
+
+  useEffect(() => {
+    if (isCopied) {
+      setCopyButtonClass('copy-button__copied');
+      setCopyButtonText('Copied!');
+      setTimeout(() => {
+        setIsCopied(false);
+      }, 1000);
+    } else {
+      setCopyButtonClass('copy-button');
+      setCopyButtonText('Copy Description');
+    }
+  }, [isCopied]);
+
   return (
-    <>
+    <div className="flex-gap">
       <h1>Roman Coin Description Generator</h1>
-      <div className='select'>
-        <Select
-          defaultValue={person}
-          onChange={setPerson}
-          options={peopleDescriptions}
-        />
-        <Select
-          defaultValue={coinType}
-          onChange={setCoinType}
-          options={coinTypes}
-        />
-        <Select
-          defaultValue={grade}
-          onChange={setGrade}
-          options={grades}
-        />
-        <Select
-          defaultValue={flaws}
-          onChange={setFlaws}
-          options={flawDescriptions}
-          isMulti
-        />
+      <div className='select-container'>
+        <div className='select'>
+          Person on the coin:
+          <Select
+            defaultValue={person}
+            onChange={setPerson}
+            options={peopleDescriptions}
+          />
+        </div>
+        <div className='select'>
+          Coin type:
+          <Select
+            defaultValue={coinType}
+            onChange={setCoinType}
+            options={coinTypes}
+          />
+        </div>
+        <div className='select'>
+          Grade:
+          <Select
+            defaultValue={grade}
+            onChange={setGrade}
+            options={grades}
+          />
+        </div>
+        <div className='select'>
+          Flaws, if any:
+          <Select
+            defaultValue={flaws}
+            onChange={setFlaws}
+            options={flawDescriptions}
+            isMulti
+          />
+        </div>
       </div>
-      <button onClick={() => generateDescription()}>
-        Generate description
-      </button>
+      <div className="button-container">
+        <button onClick={() => generateDescription()}>
+          Generate description
+        </button>
+        <button
+          className={copyButtonClass}
+          onClick={copyButtonOnClick}
+        >
+          {copyButtonText}
+        </button>
+      </div>
       <div className='description'>
         {description}
-      </div>
-    </>
+    </div>
   )
 }
 
